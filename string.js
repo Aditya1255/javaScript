d745de504c64205e0ba354830f9836719ce435e2
Aditya kumar <92204349+Aditya1255@users.noreply.github.com>
2024-03-16 02:30:44 +0530

    Create string.js

---
+++ string.js
@@ -0,0 +1,83 @@
+
+ //1........................................... UPPERCASE TO LOWERCASE
+// let str = "ADITYA";
+// let ans = "";
+
+// for(let i =0; i< str.length; ++i){
+//     let temp = str[i];
+//     temp = temp.charCodeAt(0);
+//     temp = temp + 32;
+//     temp = String.fromCharCode(temp);
+//     ans += temp;
+// }
+
+// console.log(ans)
+
+
+
+// 2............................................LOWERCASE TO UPPERCASE
+// let str = "aditya";
+// let ans = "";
+
+// for(let i =0; i< str.length; ++i){
+//     let temp = str[i];
+//     temp = temp.charCodeAt(0);
+//     temp = temp - 32;
+//     temp = String.fromCharCode(temp);
+//     ans += temp;
+// }
+
+// console.log(ans)
+
+
+
+//3..............................................CAPTIALIZE STRINGS
+// let str = "AdiTYa KumAr siNgH";
+// let ans = "";
+
+// for(let i =0; i< str.length; ++i){
+//     let temp = str[i];
+//     let temp1 = temp.charCodeAt(0);
+//     if( temp1 >= 97 && temp1 <= 127){
+//         temp1 = temp1 - 32;
+//         temp1 = String.fromCharCode(temp1);
+//         ans += temp1;
+//     }
+//     else{
+//         ans += temp;
+//     }
+// }
+
+// console.log(ans)
+
+
+// 4..............................................FIRST INDEX OF CHARACTER IN STRINGS
+// function index_Of(str , target){
+//     for(let i =0; i<str.length; ++i){
+//         if(str[i] == target){
+//             return i;
+//             break;
+//         }
+//     }
+// }
+// let ans = index_Of("Aditya Kumar Singh", "a");
+
+// console.log(ans)
+
+
+// 6..............................................LAST INDEX OF CHARACTER IN STRINGS
+// function index_Of(str , target){
+//     let ans = -1
+//     for(let i =0; i<str.length; ++i){
+//         if(str[i] == target){
+//             ans = i;
+//         }
+//     }
+//     return ans;
+// }
+// let ans = index_Of("Aditya Kumar Singh", "a");
+
+// console.log(ans)
+
+
+
